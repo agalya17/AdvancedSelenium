3531066886487ee221ff8f83ba9b595a3123736f
qualian pc <qualian pc@QT2021LP98>
2025-08-28 07:20:32 +0530

    Anushka push new changes

---
+++ NewProject/src/test/java/Sample/AnushkaSharma.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class AnushkaSharma {
+
+	public static void main(String[] args) {
+		System.out.println("New programmar");
+		System.out.println("New programmar");
+		System.out.println("New programmar");
+	}
+
+}
